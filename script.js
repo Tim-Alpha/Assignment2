b6f9beee3e876ecda63f7bca3e9e0c242fa3e6da
SachinKinha <112786050+nk-code-lab@users.noreply.github.com>
2023-04-07 16:03:31 +0530

    first commit
    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function alertFun1() {
+    alert("Know more function get clicked!\nThanks for visiting...")
+}
+function alertFun2() {
+    alert("Govt fee function get clicked!\nGovt fee are not included in the subscription.\nThanks for visiting...")
+}
